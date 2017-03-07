df2be47e083317e89faa28956a999cdb01f06e6e
Ming-Hsi Li <Ming-Hsi Li>
2017-03-07 19:54:34 +0800

    git test

---
+++ HelloUnity/Assets/HelloScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloScript : MonoBehaviour {
+
+    private int updateCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello Unity Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Hello Unity Update Loop i=" + (updateCounter++));
+	}
+}
